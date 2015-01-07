43427ca9ad1e45a28860bca050251395f24eda24
Ray Hua Wu <dzaefn@mit.edu>
2015-01-06 22:50:08 -0500

    adjusting stuff (not completed yet)

--- gravity.js
+++ gravity.js
@@ -2,16 +2,19 @@ var CANVAS_HORZ_SIZE = 2500;
 var CANVAS_VERT_SIZE = 800;
 var LEFT_MARGIN = 60;
 var BOT_MARGIN = 60;
-var TOP_MARGIN = 60;
+var TOP_MARGIN = 100;
 var RIGHT_MARGIN = 60;
-var LINE_WIDTH = 3;
+var LINE_WIDTH = 5;
+var EDGE_LINE_WIDTH = 3;
 var TEXT_VERT_SIZE = 12
 var canvas = Raphael(0,0,CANVAS_HORZ_SIZE,CANVAS_VERT_SIZE);
 
 var secants = {'tetraVertex':3,'tetraEdge':Math.sqrt(3),'cubeVertex':Math.sqrt(3),'cubeEdge':Math.sqrt(2),'octaVertex':Math.sqrt(3),'octaEdge':Math.sqrt(3/2),'dodecaVertex':Math.sqrt(15-6*Math.sqrt(5)),'dodecaEdge':Math.sqrt((5-Math.sqrt(5))/2),'icosaVertex':Math.sqrt(15-6*Math.sqrt(5)),'icosaEdge':(Math.sqrt(15)-Math.sqrt(3))/2};
-var colors = {'tetraVertex':'#FF0000','tetraEdge':'#FF0000','cubeVertex':'#008000','cubeEdge':'#008000','octaVertex':'#008000','octaEdge':'#008000','dodecaVertex':'#0000FF','dodecaEdge':'#0000FF','icosaVertex':'#0000FF','icosaEdge':'#0000FF'};
+var colors = {'tetraVertex':'#FF0000','tetraEdge':'#FF0000','cubeVertex':'#00FF00','cubeEdge':'#00FF00','octaVertex':'#00FF00','octaEdge':'#00FF00','dodecaVertex':'#0000FF','dodecaEdge':'#0000FF','icosaVertex':'#0000FF','icosaEdge':'#0000FF'};
 var fullText1 = {'tetraVertex':'Tetrahedron','tetraEdge':'Tetrahedron','cubeVertex':'Cube','cubeEdge':'Cube','octaVertex':'Octahedron','octaEdge':'Octahedron','dodecaVertex':'Dodecahedron','dodecaEdge':'Dodecahedron','icosaVertex':'Icosahedron','icosaEdge':'Icosahedron'};
-var fullText2 = {'tetraVertex':'Vertex','tetraEdge':'Edge Center','cubeVertex':'Vertex','Vertex':'EdgeCenter','octaVertex':'Vertex','octaEdge':'Edge Center','dodecaVertex':'Vertex','dodecaEdge':'Edge Center','icosaVertex':'Vertex','icosaEdge':'Edge Center'};
+var fullText2 = {'tetraVertex':'Vertex','tetraEdge':'Edge Center','cubeVertex':'Vertex','cubeEdge':'Edge Center','octaVertex':'Vertex','octaEdge':'Edge Center','dodecaVertex':'Vertex','dodecaEdge':'Edge Center','icosaVertex':'Vertex','icosaEdge':'Edge Center'};
+var fullText3 = {'tetraVertex':'1.231 (70.5\xB0)','tetraEdge':'0.955 (54.7\xB0)','cubeVertex':'0.955 (54.7\xB0)','cubeEdge':'0.785 (45\xB0)','octaVertex':'0.955 (54.7\xB0)','octaEdge':'0.616 (35.3\xB0)','dodecaVertex':'0.652 (37.4\xB0)','dodecaEdge':'0.554 (31.7\xB0)','icosaVertex':'0.652 (37.4\xB0)','icosaEdge':'0.365 (20.9\xB0)'};
+var textDeviation = {'tetraVertex':0,'tetraEdge':0,'cubeVertex':0,'cubeEdge':0,'octaVertex':0,'octaEdge':0,'dodecaVertex':40,'dodecaEdge':-20,'icosaVertex':40,'icosaEdge':0};
 
 /* produces string to represent a line from (x1,y1) to (x2,y2) */
 function parseLine(x1,y1,x2,y2){
@@ -26,13 +29,19 @@ function parseLine(x1,y1,x2,y2){
   return lineString;
 }
 
-var linesToDraw = ['tetraVertex','tetraEdge','cubeEdge','octaEdge','dodecaVertex','dodecaEdge','icosaEdge'];
+var linesToDraw = ['cubeVertex','tetraVertex','tetraEdge','cubeEdge','octaEdge','icosaVertex','dodecaEdge','icosaEdge'];
 
 for(var i=0;i<linesToDraw.length;i++){
   var horizDeviation = LEFT_MARGIN+Math.sqrt(Math.pow(secants[linesToDraw[i]]*(CANVAS_VERT_SIZE-BOT_MARGIN-TOP_MARGIN),2)-Math.pow(CANVAS_VERT_SIZE-BOT_MARGIN-TOP_MARGIN,2));
-  var lineDrawn = canvas.path(parseLine(LEFT_MARGIN,CANVAS_VERT_SIZE-BOT_MARGIN,horizDeviation,TOP_MARGIN)).attr({'stroke-width':LINE_WIDTH}).attr({'stroke-linecap':'round'}).attr({'stroke':colors[linesToDraw[i]]});
-  var lineLabel1 = canvas.text(horizDeviation,TOP_MARGIN-3.5*TEXT_VERT_SIZE,fullText1[linesToDraw[i]]).attr({'stroke':colors[linesToDraw[i]]});
-  var lineLabel2 = canvas.text(horizDeviation,TOP_MARGIN-2.5*TEXT_VERT_SIZE,fullText2[linesToDraw[i]]).attr({'stroke':colors[linesToDraw[i]]});
+  if(linesToDraw[i].contains('Edge')){
+    var lineDrawn = canvas.path(parseLine(LEFT_MARGIN,CANVAS_VERT_SIZE-BOT_MARGIN,horizDeviation,TOP_MARGIN)).attr({'stroke-width':LINE_WIDTH-2}).attr({'stroke-linecap':'round'}).attr({'stroke':colors[linesToDraw[i]]});
+  }else{
+    var lineDrawn = canvas.path(parseLine(LEFT_MARGIN,CANVAS_VERT_SIZE-BOT_MARGIN,horizDeviation,TOP_MARGIN)).attr({'stroke-width':LINE_WIDTH}).attr({'stroke-linecap':'round'}).attr({'stroke':colors[linesToDraw[i]]});
+  }
+  var lineLabel1 = canvas.text(horizDeviation+textDeviation[linesToDraw[i]],TOP_MARGIN-4*TEXT_VERT_SIZE,fullText1[linesToDraw[i]]).attr({'stroke':colors[linesToDraw[i]]});
+  var lineLabel2 = canvas.text(horizDeviation+textDeviation[linesToDraw[i]],TOP_MARGIN-3*TEXT_VERT_SIZE,fullText2[linesToDraw[i]]).attr({'stroke':colors[linesToDraw[i]]});
+  var lineLabel3 = canvas.text(horizDeviation+textDeviation[linesToDraw[i]],TOP_MARGIN-2*TEXT_VERT_SIZE,fullText3[linesToDraw[i]]).attr({'stroke':colors[linesToDraw[i]]});
+  var lineLabel4 = canvas.text(horizDeviation+textDeviation[linesToDraw[i]],TOP_MARGIN-TEXT_VERT_SIZE,'from the normal').attr({'stroke':colors[linesToDraw[i]]});
 }
 var normal = canvas.path(parseLine(LEFT_MARGIN,CANVAS_VERT_SIZE-BOT_MARGIN,LEFT_MARGIN,TOP_MARGIN)).attr({'stroke-width':LINE_WIDTH}).attr({'stroke-linecap':'round'}); 
 var ground = canvas.path(parseLine(LEFT_MARGIN,CANVAS_VERT_SIZE-BOT_MARGIN,CANVAS_HORZ_SIZE-RIGHT_MARGIN,CANVAS_VERT_SIZE-BOT_MARGIN)).attr({'stroke-width':LINE_WIDTH}).attr({'stroke-linecap':'round'}); 
